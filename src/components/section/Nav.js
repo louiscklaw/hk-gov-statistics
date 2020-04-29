17ebcec4980244ffde30da72d8b611af761db9f0
louiscklaw <louiscklaw@gmail.com>
2020-04-29 19:10:24 +0800

    update nav bar in the middle,

--- src/components/section/Nav.js
+++ src/components/section/Nav.js
@@ -8,9 +8,21 @@ class TopBar extends Component{
       <nav className="navbar">
         <div className="container">
           <div className="navbar-brand">
-
-
-
+            <a className="navbar-item" href="../">
+              {/* <img src="http://bulma.io/images/bulma-type-white.png" alt="Logo" /> */}
+              <h3 className="title is-3">
+                LouisLabs
+                <span style={{
+                  fontSize: '0.5em',
+                  paddingLeft: '10px'
+                }}>last update: xxxxxx</span>
+              </h3>
+            </a>
+              <span className="navbar-burger burger" data-target="navbarMenu">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
           </div>
           <Link to={`/`}>Index</Link>
           <Link to={`/about`}>about</Link>
