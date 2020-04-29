6d8ed557729b9a6007e9ed2110e1351755ccb302
louiscklaw <louiscklaw@gmail.com>
2020-04-29 20:39:57 +0800

    in the middle, last stop at apilist.js, try to fix using context inside class,

--- src/components/section/ApiList.js
+++ src/components/section/ApiList.js
@@ -1,12 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 
 import ApiListCard from '../ApiListCard';
 
 import { checkDevelop } from '../../common';
 
+import {GlobalContext} from '../../context/GlobalContext';
+
+
+
 class ApiList extends Component{
+
+  filterUsingKeyword(keywords, json_in){
+    if (keywords.length > 0){
+      try {
+        let having_all_keywords = !(keywords
+          .map(kword => this.plaintextSearch(json_in, kword) > -1 )
+          .includes(false))
+          return having_all_keywords
+      } catch (err) {
+        console.error('error',typeof(keywords))
+      }
+    }else{
+      // TODO: ???
+      return true
+    }
+  }
+
+
   render(){
-    let api_cards = 'test api_cards';
+    let all_api_list = 1;
+    let search_string = 2;
+    // console.log(useContext(GlobalContext));
+
+    if (checkDevelop()){
+      console.log('this.props', this.props)
+    }
+
+    let api_cards = Object.keys(all_api_list)
+      .filter( k => { return this.filterUsingKeyword([search_string], all_api_list[k]) })
+      .map( open_api => {
+        // let{groups, organization, resources} = all_api_list[open_api].result
+
+        return this.getCard(all_api_list[open_api], open_api)
+      })
+      .sort()
+
+      // this.props.update_api_found(api_cards.length)
 
     return(
       <div className="container">
