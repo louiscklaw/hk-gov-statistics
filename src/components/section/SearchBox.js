c36494e66efa737138c9ecfaf6e4ca3aa36d5e0c
louiscklaw <louiscklaw@gmail.com>
2020-04-29 19:23:34 +0800

    update search box,

--- src/components/section/SearchBox.js
+++ src/components/section/SearchBox.js
@@ -10,7 +10,30 @@ class SearchBox extends Component{
           <div className="hero-head">
             <Nav />
           </div>
-          searchbox here
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">香港統計數字</h1>
+              <p className="control has-icons-left">
+                <input
+                  id="search_input"
+                  className="input is-rounded is-large"
+                  type="text"
+                  placeholder="Search"
+                  onChange={(e)=>{this.handleOnChange(e)}}
+                  value={this.props.search_string}
+                  />
+
+                <span className="icon is-small is-left">
+                  <i className="fas fa-binoculars"></i>
+                </span>
+              </p>
+              <p className="subtitle is-6 total-api-count">
+                {this.getTotalApiCount()} API in dictionary{this.showApiFound()}
+              </p>
+            </div>
+          </div>
+
+
         </section>
 
       </div>
